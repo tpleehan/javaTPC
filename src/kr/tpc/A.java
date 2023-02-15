f24ae26133693b3eabb313e0f4dd2508a5907e0b
tpleehan <tpleehan@gmail.com>
2023-02-15 23:07:47 +0900

    feat: Java TPC(생각하고, 표현하고, 코딩하고) - Part2 - Object 클래스의 활용

---
+++ src/kr/tpc/A.java
@@ -0,0 +1,7 @@
+package kr.tpc;
+
+public class A {
+	public void go() {
+		System.out.println("A 클래스의 go() 메서드 입니다.");
+	}
+}
---
+++ src/kr/tpc/B.java
@@ -0,0 +1,7 @@
+package kr.tpc;
+
+public class B {
+	public void go() {
+		System.out.println("B 클래스의 go() 메서드 입니다.");
+	}
+}

3916374de711c48eeedb2a24f02586ff8cdbac06
tpleehan <tpleehan@gmail.com>
2023-02-16 01:04:34 +0900

    feat: Java TPC(생각하고, 표현하고, 코딩하고) - Part2 - package(패키지)가 뭐예요?

---
+++ src/TPC33.java
@@ -0,0 +1,12 @@
+// import kr.inflearn.MyClass;
+
+import kr.inflearn.MyClass;
+
+public class TPC33 {
+	public static void main(String[] args) {
+		MyClass my = new MyClass();
+		int v = my.sum(1, 10);
+		System.out.println(v);
+
+	}
+}

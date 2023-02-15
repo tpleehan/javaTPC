1c2fac1f446d257814d32c0cd33cfc5143234964
tpleehan <tpleehan@gmail.com>
2023-02-15 22:07:53 +0900

    feat: Java TPC(생각하고, 표현하고, 코딩하고) - Part2 - 부모가 있어서 너무 좋아!

--- src/kr/poly/TV.java
+++ src/kr/poly/TV.java
@@ -1,15 +1,28 @@
 package kr.poly;
 
 public class TV implements RemoCon {
+	int currCH = 70;
 
 	@Override
 	public void chUp() {
-		System.out.println("TV의 채널이 올라간다.");
+		if (currCH < RemoCon.MAXCH) {
+			currCH++;
+			System.out.println("TV의 채널이 올라간다. : " + currCH);
+		} else {
+			currCH = 1;
+			System.out.println("TV의 채널이 올라간다. : " + currCH);
+		}
 	}
 
 	@Override
 	public void chDown() {
-		System.out.println("TV의 채널이 내려간다.");
+		if (currCH > RemoCon.MINCH) {
+			currCH--;
+			System.out.println("TV의 채널이 내려간다. : " + currCH);
+		} else {
+			currCH = 100;
+			System.out.println("TV의 채널이 내려간다. : " + currCH);
+		}
 	}
 
 	@Override

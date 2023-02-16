bd795a97a1c6db6487137651ef444b08b6f4001b
tpleehan <tpleehan@gmail.com>
2023-02-16 12:06:22 +0900

    feat: Java TPC(생각하고, 표현하고, 코딩하고) - Part2 - ArrayList 흉내내기(Object[])

---
+++ src/TPC37.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+import kr.tpc.A;
+import kr.tpc.B;
+
+public class TPC37 {
+	public static void main(String[] args) {
+		// ObjectArray arr = new ObjectArray(5);
+		ArrayList arr = new ArrayList(5); // Object[], 배열의 길이에 제약이 없다.
+		arr.add(new A());
+		arr.add(new B());
+		arr.add(new A());
+		arr.add(new A());
+		arr.add(new B());
+		arr.add(new A());
+		arr.add(new A());
+		arr.add(new A());
+		arr.add(new A());
+
+		for (int i = 0; i < arr.size(); i++) {
+			Object o = arr.get(i);
+			if (o instanceof A) {
+				((A)o).go();
+			} else {
+				((B)o).go();
+			}
+		}
+
+	}
+}
